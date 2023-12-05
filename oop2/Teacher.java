7ab91701a6d652f544494afcca00eb25509ce6e9
Md. Fazle Rabbi Riyad <riyadd78666@gmail.com>
2023-12-05 23:58:36 +0600

    More About Java OOP

---
+++ oop2/Teacher.java
@@ -0,0 +1,17 @@
+
+package oop2;
+
+
+public class Teacher {
+        String name,gender;
+        int number;
+        
+        void DisplayrInformation(){
+                
+        System.out.println("Gender = "+gender);
+        System.out.println("Name= "+name);
+        System.out.println("Number = "+number);
+            System.out.println();
+        }
+    
+}
---
+++ oop2/Test.java
@@ -0,0 +1,26 @@
+
+package oop2;
+
+
+public class Test {
+      public static void main(String[] args) {
+        Teacher teacher1 = new Teacher();
+        teacher1.gender = "Male";
+        teacher1.name = "Fazle Rabbi";
+        teacher1.number = 1307813956;
+        teacher1.DisplayrInformation();
+        
+        Teacher teacher2 = new Teacher();
+        teacher2.gender = "Female";
+        teacher2.name = "Anika";
+        teacher2.number = 1829415010;
+        teacher2.DisplayrInformation();
+            
+       
+      
+      
+        
+    }
+    
+    
+}
