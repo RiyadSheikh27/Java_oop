32871066eeb5d8975dd12762523d9269e6e0d441
Md. Fazle Rabbi Riyad <riyadd78666@gmail.com>
2023-12-06 00:00:06 +0600

    More About Object Oriented

---
+++ oop4/Test.java
@@ -0,0 +1,19 @@
+
+package oop4;
+
+
+public class Test {
+      public static void main(String[] args) {
+        Teacher teacher1 = new Teacher("Male", "Fazle Rabbi",1307813956);
+        teacher1.DisplayrInformation();
+        
+        Teacher teacher2 = new Teacher("Female","Anika",1829415010);
+        teacher2.DisplayrInformation();
+            
+       
+      
+      
+        
+    }
+    
+}
